86f3811b21006861ef54b35a8ba72b66580d5534
williamjingjiefeng <jingjie_feng@hotmail.com>
2021-05-20 09:03:23 +1000

    A major update to README.md

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -4,8 +4,8 @@ const AboutPage = () => (
   <div>
     <h2>About</h2>
     <p>
-      This app uses React, Redux, React Router, and many other helpful
-      libraries.
+      This application uses React, Redux, Redux thunk, React context provider for global data management,
+      React Router, and many other helpful libraries.
     </p>
   </div>
 );
