961d6718ae3ecf797eb2903a562604b9cf11fe3c
williamjingjiefeng <jingjie_feng@hotmail.com>
2021-08-10 17:41:28 +1000

    Start to work on Jest test

--- src/components/songs/SongForm.Snapshots.test.js
+++ src/components/songs/SongForm.Snapshots.test.js
@@ -4,31 +4,33 @@ import renderer from "react-test-renderer";
 import { songs, singers, albums } from "../../../tools/mockMusics";
 
 it("sets submit button label 'Saving...' when saving is true", () => {
-  const tree = renderer.create(
-    <SongForm
-      song={songs[0]}
-      singers={singers}
-      albums={albums}
-      onSave={jest.fn()}
-      onChange={jest.fn()}
-      saving
-    />
-  );
+    const tree = renderer.create(
+        <SongForm
+            song={songs[0]}
+            singers={singers}
+            albums={albums}
+            onSave={jest.fn()}
+            onChange={jest.fn()}
+            saving
+            errors={{}}
+        />
+    );
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
 });
 
 it("sets submit button label 'Save' when saving is false", () => {
-  const tree = renderer.create(
-    <SongForm
-      song={songs[0]}
-      singers={singers}
-      albums={albums}
-      onSave={jest.fn()}
-      onChange={jest.fn()}
-      saving={false}
-    />
-  );
+    const tree = renderer.create(
+        <SongForm
+            song={songs[0]}
+            singers={singers}
+            albums={albums}
+            onSave={jest.fn()}
+            onChange={jest.fn()}
+            saving={false}
+            errors={{}}
+        />
+    );
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
 });
